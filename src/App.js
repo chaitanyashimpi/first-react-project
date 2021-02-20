2693283aadd4a32c993ed94f2bf51e5ea8cc3aac
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2021-02-20 21:22:02 +0530

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -2,34 +2,31 @@
 // throw new Error('lol');
 
 const Pet = ({ name, animal, bread }) => {
-    return React.createElement("div",{},[
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, bread)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, bread),
+  ]);
 };
 
-const App = props => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { 
-                name: 'Luna', 
-                animal: 'Dog', 
-                bread: 'Havanese'
-            }),
-            React.createElement(Pet, { 
-                name: 'Pepper', 
-                animal: 'Bird', 
-                bread: 'Cocktatiel'
-            }),
-            React.createElement(Pet, { name: 'Doink', animal: 'Cat', bread: 'Mixed'})
-        ]
-    );
+const App = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      bread: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      bread: "Cocktatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      bread: "Mixed",
+    }),
+  ]);
 };
-ReactDOM.render(
-    React.createElement(App), 
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
